02f64141d6a16a55d170cae30fadf4e6101a7c0c
Daniel <smileqiao1988@yahoo.com>
2020-02-05 20:53:31 +0000

    debug

--- models/deepCrispr_OT/config.py
+++ models/deepCrispr_OT/config.py
@@ -14,7 +14,7 @@ else:
 
 # data speficic name, for example, if file name is K562_CN, then data_specific is _CN
 data_specific = "_data" if cellline != "test" else ""
-update_features = False
+update_features = True
 
 # sequence length
 seq_start = 0
